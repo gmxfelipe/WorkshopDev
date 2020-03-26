ecaf9eab082affbac825fdf1d3daded8cbebec3c
unknown <felipeAgomes>
2020-03-26 15:24:29 -0300

    Configurando dependências do servidor

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"));
+
+server.get("/", function(req, res) {
+    return res.sendFile(__dirname + "./index.html");
+});
+
+
+
+server.listen(3333);
