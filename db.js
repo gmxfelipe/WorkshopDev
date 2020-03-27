274e4bd4e706246869ec1468c3579caf39ec9dca
unknown <felipeAgomes>
2020-03-27 13:37:11 -0300

    Erro na requisão e resposta entre métodos post e get

--- db.js
+++ db.js
@@ -14,28 +14,44 @@ db.serialize(function() {
         );
     `)
     // Enserir dado na tabela 
-        const query = `
-        INSERT INTO ideas(
-            image,
-            title,
-            category,
-            description,
-            link
-        ) VALUES(?,?,?,?,?);
-        `
-        const values =  [
-            "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-            "Cursos de programação",
-            "Estudo",
-            "A beleza ideal está na simplicidade calma e serena.",
-            "https://felipeagomes.github.io/"
-       ]
-        db.run(query, values, function(err) {
-            if (err) return console.log(err)
+    //     const query = `
+    //     INSERT INTO ideas(
+    //         image,
+    //         title,
+    //         category,
+    //         description,
+    //         link
+    //     ) VALUES(?,?,?,?,?);
+    //     `
+    //     const values =  [
+    //         "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    //         "Cursos de programação",
+    //         "Estudo",
+    //         "A beleza ideal está na simplicidade calma e serena.",
+    //         "https://felipeagomes.github.io/"
+    //    ]
 
-            console.log(this)
-        });
-    // Consultar dado na tabela 
+
+    //     db.run(query, values, function(err) {
+    //         if (err) return console.log(err)
+
+    //         console.log(this)
+    //     });
 
     // Deletar dado na tabela 
+    //    db.run(`DELETE FROM ideas WHERE id = ?`, [1], function (err) {
+    //        if (err) return console.log(err)
+
+    //        console.log("Deletei", this)
+    //    });
+
+    // Consultar dado na tabela 
+    //    db.all(`SELECT * FROM ideas`, function(err, rows) {
+    //        if (err) return console.log(err)
+
+    //        console.log(rows)
+    //    });
+
 });
+
+module.exports = db
